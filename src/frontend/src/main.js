208a91714febe66907c075b0d78c80d423dc3f2e
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2019-04-03 15:42:35 -0300

    send X-CSRFToken

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,21 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Cookies from 'js-cookie'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:8000/graphql/' })
+// Install the vue plugin
+Vue.use(VueApollo)
 
 // apollo client setup
+const csrftoken = Cookies.get('csrftoken')
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000/graphql/',
+  headers: {
+    "X-CSRFToken": csrftoken
+  }
+})
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
-
-// Install the vue plugin
-Vue.use(VueApollo)
 
 // Apollo provider init
 const apolloProvider = new VueApollo({
