386c6a4c948edb0a811b04c8706e206b42873592
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2019-04-03 10:59:36 -0300

    adds new frontend, basic minimal graphql project created with `graphql-cli`

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <div class="App-header">
+      <h3>Welcome to Django + Django Graphene + Vue Apollo minimal boilerplate</h3>
+      <template v-if="loading > 0">
+        <div class="flex w-100 h-100 items-center justify-center pt7">
+          <div>Loading...</div>
+        </div>
+      </template>
+      <template v-if="users.length">
+        <h3>User list</h3>
+        <p :key="idx" v-for="(user, idx) in users">{{ user.email }}</p>
+      </template>
+      <template v-else>
+        <template v-if="error">
+          <div class="flex w-100 h-100 items-center justify-center pt7">
+            <div>An unexpected error occurred</div>
+          </div>
+        </template>
+        <template v-else>
+          <h3>{hello}</h3>
+        </template>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  import gql from 'graphql-tag'
+  const HELLO_QUERY = gql(`
+    query {
+      users {
+        email
+      }
+    }
+  `)
+
+  export default {
+    name: 'app',
+
+    // data props to component
+    data: () => ({
+      hello: String,
+      loading: 0,
+      error: String,
+      users: [],
+    }),
+
+    // Apollo GraphQL
+    apollo: {
+      users: {
+        query: HELLO_QUERY,
+        loadingKey: 'loading',
+        error(error) {
+          console.error('We\'ve got an error!', error)
+          this.error = error
+        }
+      },
+    }
+  }
+</script>
+
+<style>
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+
+.App {
+  text-align: center;
+}
+
+.App-logo {
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+}
+
+.App-header {
+  background-color: white;
+  height: 250px;
+  padding: 20px;
+  color:black;
+}
+
+.App-title {
+  font-size: 1.5em;
+}
+
+.App-intro {
+  font-size: large;
+  text-align: 'center';
+}
+
+h3 {
+  text-align: center;
+  padding:10px;
+}
+
+@keyframes App-logo-spin {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
+
+
+</style>
