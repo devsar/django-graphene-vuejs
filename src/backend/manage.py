26b2e3f9abd9281c44da846d13c14f944aa188dc
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2019-04-09 15:53:05 -0300

    removes old project

--- src/backend/manage.py
+++ src/backend/manage.py
@@ -5,11 +5,16 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line, get_commands
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    commands = list(get_commands().keys()) + ['help', '--help']
+    if sys.argv[1] in commands:
+        execute_from_command_line(sys.argv)
+    else:
+        import subprocess
+        subprocess.run(sys.argv[1:])
