e31136dc70537787dfb5e12405e1e5e89414b65f
Emiliano Dalla Verde Marcozzi <6564766d@gmail.com>
2019-04-10 09:48:51 -0300

    static files are working

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'backend/static'),os.path.join(BASE_DIR, 'static'),)
 
 GRAPHENE = {
     'SCHEMA': 'backend.schema.schema',
